4e16d7977c93f2e7704a6072fc8f2570efa3daef
myfreeweb <floatboth@me.com>
2014-04-03 18:06:54 +0400

    use new?

--- lib/spree/yandex_market/config.rb
+++ lib/spree/yandex_market/config.rb
@@ -1,19 +1,17 @@
 module Spree
   module YandexMarket
-    # Singleton class to access the advanced cart configuration object (YandexMarketConfiguration.first by default) and it's preferences.
     #
     # Usage:
     #   Spree::YandexMarket::Config[:foo]                  # Returns the foo preference
     #   Spree::YandexMarket::Config[]                      # Returns a Hash with all the google base preferences
-    #   Spree::YandexMarket::Config.instance               # Returns the configuration object (YandexMarketConfiguration.first)
+    #   Spree::YandexMarket::Config.instance               # Returns the configuration object (YandexMarketConfiguration.new)
     #   Spree::YandexMarket::Config.set(preferences_hash)  # Set the advanced cart preferences as especified in +preference_hash+
     class Config
       include Singleton
 
       class << self
         def instance
-          return nil unless ActiveRecord::Base.connection.tables.include?('spree_configurations')
-          Spree::YandexMarketConfiguration.first_or_create
+          Spree::YandexMarketConfiguration.new
         end
       end
     end
