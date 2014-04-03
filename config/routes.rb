e392650e2f15ec8360e0815b0a1513ab509098af
myfreeweb <floatboth@me.com>
2014-04-03 17:41:01 +0400

    fix match routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Spree::Core::Engine.add_routes do
   namespace :admin do
     resource :yandex_market_settings do
       member do
-        match :general
-        match :currency
-        match :export_files
-        match :ware_property
+        match :general, via: [:get, :put]
+        get :currency
+        get :export_files
+        get :ware_property
         get :run_export
       end
     end
