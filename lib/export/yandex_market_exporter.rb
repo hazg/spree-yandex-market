c25ecf336375ac9e76859f6580e243d773bdacca
myfreeweb <floatboth@me.com>
2014-04-03 18:53:01 +0400

    picture url should not be escaped

--- lib/export/yandex_market_exporter.rb
+++ lib/export/yandex_market_exporter.rb
@@ -82,7 +82,7 @@ module Export
       xml.price product.price
       xml.currencyId @currencies.first.first
       xml.categoryId cat.id
-      xml.picture path_to_url(CGI.escape(product.images.first.attachment.url(:product, false))) unless product.images.empty?
+      xml.picture path_to_url(product.images.first.attachment.url(:product, false)) unless product.images.empty?
     end
     
     def individual_xml(xml, product, cat, product_properties = {})
