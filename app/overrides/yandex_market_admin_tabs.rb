3d9893de91a895c3e5c306726763be6477362b94
myfreeweb <floatboth@me.com>
2014-07-01 22:54:33 +0400

    fix icon

--- app/overrides/yandex_market_admin_tabs.rb
+++ app/overrides/yandex_market_admin_tabs.rb
@@ -1,6 +1,6 @@
 Deface::Override.new( :virtual_path => "spree/admin/shared/_menu",
                       :name => "add-yandex-market-admin-tab",
                       :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                      :text => "<%= tab :yandex_market, :icon => 'icon-shopping-cart', :url => spree.admin_yandex_market_settings_path, :match_path => '/yandex_market_settings' %>",
+                      :text => "<%= tab :yandex_market, :icon => 'shopping-cart', :url => spree.admin_yandex_market_settings_path, :match_path => '/yandex_market_settings' %>",
                       :disabled => false
                     )
