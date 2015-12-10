1784559ad5829e01b242fb51bf8678aa98503f33
Greg V <greg@unrelenting.technology>
2015-12-10 20:17:35 +0300

    fix tab

--- app/overrides/yandex_market_admin_tabs.rb
+++ app/overrides/yandex_market_admin_tabs.rb
@@ -1,6 +1,5 @@
-Deface::Override.new( :virtual_path => "spree/admin/shared/_menu",
-                      :name => "add-yandex-market-admin-tab",
-                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                      :text => "<%= tab :yandex_market, :icon => 'shopping-cart', :url => spree.admin_yandex_market_settings_path, :match_path => '/yandex_market_settings' %>",
-                      :disabled => false
-                    )
+Deface::Override.new(virtual_path: "spree/layouts/admin",
+                     name: "add-yandex-market-admin-tab",
+                     insert_bottom: "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     text: "<ul class='nav nav-sidebar'><%= tab :yandex_market, icon: 'shopping-cart', url: spree.admin_yandex_market_settings_path, match_path: '/yandex_market_settings' %></ul>",
+                     disabled: false)
